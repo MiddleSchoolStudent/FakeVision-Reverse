e34e0bb1236591b619c3fb0b872339c836787323
MiddleSchoolStudent <middleschoolstudent00@gmail.com>
2024-12-23 23:34:23 -0500

    t3

---
+++ 1.8.8/detections/VMs/t3.js
@@ -0,0 +1,142 @@
+// @ts-check
+
+const _0x40fc4e = {
+    alg: "RSA-OAEP-256",
+    e: "AQAB",
+    ext: true,
+    key_ops: ["encrypt"],
+    kty: "RSA",
+    n: "8DJNossKBi-kNHAmIckUEs2ceEm32xlIwjqEtJ4gtxsLHdSa8HLs2SM6tFvxLQCkq9B_dWkZ1U1B0iH5oy4EaAOqC-NhZDh_UWTLRvoj9bHFFGFhRTOG80ztY-aeGuSeE5q3k86t9dgBA3v-BBW9u_" + "ISuOd" + "_hJvb" + "6Z8kZ" + "mheak" + "74uXR" + "FTKkj" + "O9Saw" + "Za-9c" + "KC82l" + "ucYQe" + "HPBWi" + "qg7F8" + "o15TS" + "KtBqa" + "8KD5_" + "RYel3" + "BZpUa" + "N1mdQ" + "IO0HH" + "iFbxe" + "b2lLX" + "cPlXL" + "1cSGD" + "1bOeQ" + "J75i_" + "0Sl50" + "DHI6v" + "M7SZp" + "xRYc6" + "dVO4t" + "rG05b" + "GfNoQ" + "tmbww" + "_UXzc" + "OEi4L" + "ZI-4E" + "d42oT" + "QxP3r" + "Tw"
+};
+async function _0x11dcd4(_0x11a447) {
+    const _0x3f8ee7 = crypto.getRandomValues(new Uint8Array(12));
+    const _0x2594c9 = {
+        name: "AES-GCM",
+        ["lengt" + "h"]: 0x100
+    };
+    const _0x531e04 = await crypto.subtle.generateKey(_0x2594c9, true, ["encrypt", "decrypt"]);
+    const _0x1e0404 = await crypto.subtle.exportKey("raw", _0x531e04);
+    const _0xe6b838 = await _0xfedd67(_0x1e0404, _0x40fc4e);
+    const _0x42eed8 = JSON.stringify(_0x11a447);
+    const _0x20faa3 = new TextEncoder().encode(_0x42eed8);
+    const _0x4dd515 = {
+        name: "AES-GCM",
+        iv: _0x3f8ee7
+    };
+    const _0x1dbce0 = await crypto.subtle.encrypt(_0x4dd515, _0x531e04, _0x20faa3);
+    const _0x1c54ee = btoa(String.fromCharCode(...new Uint8Array(_0x1dbce0)));
+    const _0x459023 = btoa(String.fromCharCode(...new Uint8Array(_0x3f8ee7)));
+    const _0x5245ba = btoa(String.fromCharCode(...new Uint8Array(_0xe6b838)));
+    return [_0x1c54ee, _0x459023, _0x5245ba];
+}
+async function _0xfedd67(_0x1ba8b0, _0x390320) {
+    const _0x117d88 = {
+        name: "SHA-256"
+    };
+    const _0x3a7fc5 = {
+        name: "RSA-OAEP",
+        hash: _0x117d88
+    };
+    const _0x294202 = await crypto.subtle.importKey("jwk", _0x390320, _0x3a7fc5, true, ["encrypt"]);
+    const _0x5f3a5b = {
+        name: "RSA-OAEP"
+    };
+    return await crypto.subtle.encrypt(_0x5f3a5b, _0x294202, _0x1ba8b0);
+}
+const _0x4eb91b = document.createElement("video");
+_0x4eb91b.id = "videoElement";
+const _0x337290 = {};
+function _0x11f000() {
+    const _0x361664 = ["AEP-2", "toStr", "error", "2721006myKNAx", "name", "t9dgB", "ISuOd", "2825gpJioq", "reque", "N1mdQ", "lengt", "2342637OUosJp", "gener", "9474ELexJB", "IO0HH", "raw", "n (fu", "bind", "b2lLX", "5878523XzdmIQ", "com.w", ")+)+)", "2400488kMvdLD", "nstru", "apply", "192XADEca", "tion", "fromC", "1127172aWNqZU", "initD", "log", "ing", "1552906IBEIpN", "hRoWV", "CvXhz"];
+    _0x11f000 = function () {
+        return _0x361664;
+    };
+    return _0x11f000();
+}
+_0x337290.contentType = "video/mp4; codecs=\"avc1.42E01E\"";
+_0x337290.robustness = "SW_SECURE_CRYPTO";
+const _0x20dc5b = {
+    initDataTypes: ["cenc"],
+    videoCapabilities: [_0x337290]
+};
+const _0x12ef8c = [_0x20dc5b];
+function _0x36dc04() {
+    const _0x2e5b5d = function () {
+        let _0x552ea5 = true;
+        return function (_0x55618c, _0x2b0e57) {
+            const _0x33bffc = _0x552ea5 ? function () {
+                if (_0x2b0e57) {
+                    const _0x46930a = _0x2b0e57.apply(_0x55618c, arguments);
+                    _0x2b0e57 = null;
+                    return _0x46930a;
+                }
+            } : function () { };
+            _0x552ea5 = false;
+            return _0x33bffc;
+        };
+    }();
+    const _0x212776 = function () {
+        let _0x379f40 = true;
+        return function (_0x2a315a, _0x5623f7) {
+            const _0x209aab = _0x379f40 ? function () {
+                if (_0x5623f7) {
+                    const _0xc46b52 = _0x5623f7.apply(_0x2a315a, arguments);
+                    _0x5623f7 = null;
+                    return _0xc46b52;
+                }
+            } : function () { };
+            _0x379f40 = false;
+            return _0x209aab;
+        };
+    }();
+    const _0x59eb9f = _0x212776(this, function () {
+        const _0x48ac8f = function () {
+            let _0x33c72d;
+            try {
+                _0x33c72d = Function("return (function() {}.constructor(\"return this\")( ));")();
+            } catch (_0x49c0a8) {
+                _0x33c72d = window;
+            }
+            return _0x33c72d;
+        };
+        const _0x2f0e4c = _0x48ac8f();
+        const _0x3fc723 = _0x2f0e4c.console = _0x2f0e4c.console || {};
+        const _0x52785a = [];
+        for (let _0xd8b4a1 = 0; _0xd8b4a1 < _0x52785a.length; _0xd8b4a1++) {
+            const _0xe5649c = _0x212776.constructor.prototype.bind(_0x212776);
+            const _0x39f37f = _0x52785a[_0xd8b4a1];
+            const _0x3654d2 = _0x3fc723[_0x39f37f] || _0xe5649c;
+            _0xe5649c.__proto__ = _0x212776.bind(_0x212776);
+            _0xe5649c.toString = _0x3654d2.toString.bind(_0x3654d2);
+            _0x3fc723[_0x39f37f] = _0xe5649c;
+        }
+    });
+    _0x59eb9f();
+    const _0xce76c3 = performance.now();
+    if (!navigator.requestMediaKeySystemAccess) {
+        console.log("Widevine: ", false);
+        return;
+    }
+    navigator.requestMediaKeySystemAccess("com.widevine.alpha", _0x12ef8c).then(_0x3b8f2f => {
+        return _0x3b8f2f.createMediaKeys();
+    }).then(_0x36ead9 => {
+        return _0x4eb91b.setMediaKeys(_0x36ead9);
+    }).then(() => {
+        console.log("t3", true);
+        _0x11dcd4(true).then(_0x52e8e9 => {
+            localStorage.setItem("t3", JSON.stringify(_0x52e8e9));
+            const _0x2a704d = new Event("dataChanged");
+            window.dispatchEvent(_0x2a704d);
+        })["catch"](() => {
+        });
+    })["catch"](() => {
+        console.log("t3", false);
+        _0x11dcd4(false).then(_0x2f4015 => {
+            localStorage.setItem("t3", JSON.stringify(_0x2f4015));
+            const _0x5c4a8e = new Event("dataChanged");
+            window.dispatchEvent(_0x5c4a8e);
+        })["catch"](() => {
+        });
+    });
+}
+_0x36dc04();
