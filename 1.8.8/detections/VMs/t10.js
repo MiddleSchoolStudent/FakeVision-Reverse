cbb4e05380a575cdac77d18fe6c1ea73f6ac6d90
MiddleSchoolStudent <middleschoolstudent00@gmail.com>
2024-12-27 21:02:09 -0500

    no message

--- 1.8.8/detections/VMs/t10.js
+++ 1.8.8/detections/VMs/t10.js
@@ -1,106 +1,72 @@
 // @ts-check
 
 const _0x36daa1 = {
-    alg: "RSA-OAEP-256",
-    e: "AQAB",
+    alg: 'RSA-OAEP-256',
+    e: 'AQAB',
     ext: true,
-    key_ops: ["encrypt"],
-    kty: "RSA",
-    n: "8DJNossKBi-kNHAmIckUEs2ceEm32xlIwjqEtJ4gtxsLHdSa8HLs2SM6tFvxLQCkq9B_dWkZ1U1B0iH5oy4EaAOqC-NhZDh" + "_UWTL" + "Rvoj9" + "bHFFG" + "FhRTO" + "G80zt" + "Y-aeG" + "uSeE5" + "q3k86" + "t9dgB" + "A3v-B" + "BW9u_" + "ISuOd" + "_hJvb" + "6Z8kZ" + "mheak" + "74uXR" + "FTKkj" + "O9Saw" + "Za-9c" + "KC82l" + "ucYQe" + "HPBWi" + "qg7F8" + "o15TS" + "KtBqa" + "8KD5_" + "RYel3" + "BZpUa" + "N1mdQ" + "IO0HH" + "iFbxe" + "b2lLX" + "cPlXL" + "1cSGD" + "1bOeQ" + "J75i_" + "0Sl50" + "DHI6v" + "M7SZp" + "xRYc6" + "dVO4t" + "rG05b" + "GfNoQ" + "tmbww" + "_UXzc" + "OEi4L" + "ZI-4E" + "d42oT" + "QxP3r" + "Tw"
-};
+    key_ops: ['encrypt'],
+    kty: 'RSA',
+    n: '8DJNossKBi-kNHAmIckUEs2ceEm32xlIwjqEtJ4gtxsLHdSa8HLs2SM6tFvxLQCkq9B_dWkZ1U1B0iH5oy4EaAOqC-NhZDh_UWTLRvoj9bHFFGFhRTOG80ztY-aeGuSeE5q3k86t9dgBA3v-BBW9u_ISuOd_hJvb6Z8kZmheak74uXRFTKkjO9SawZa-9cKC82lucYQeHPBWiqg7F8o15TSKtBqa8KD5_RYel3BZpUaN1mdQIO0HHiFbxeb2lLXcPlXL1cSGD1bOeQJ75i_0Sl50DHI6vM7SZpxRYc6dVO4trG05bGfNoQtmbww_UXzcOEi4LZI-4Ed42oTQxP3rTw',
+}
 async function _0x4dc979(_0x320e1f) {
-    const _0x19edc6 = function () {
-        let _0x14cef2 = true;
-        return function (_0x58402d, _0x593152) {
-            const _0x3d1ce7 = _0x14cef2 ? function () {
-                if (_0x593152) {
-                    const _0x3e372a = _0x593152.apply(_0x58402d, arguments);
-                    _0x593152 = null;
-                    return _0x3e372a;
-                }
-            } : function () { };
-            _0x14cef2 = false;
-            return _0x3d1ce7;
-        };
-    }();
-    const _0x1e2750 = _0x19edc6(this, function () {
-        const _0x2eceea = function () {
-            let _0xa3a8ee;
-            try {
-                _0xa3a8ee = Function("return (function() {}.constructor(\"return this\")( ));")();
-            } catch (_0x1128ed) {
-                _0xa3a8ee = window;
-            }
-            return _0xa3a8ee;
-        };
-        const _0x3f497b = _0x2eceea();
-        const _0x48f774 = _0x3f497b.console = _0x3f497b.console || {};
-        const _0x52661e = ["log", "warn", "info", "error", "exception", "table", "trace"];
-        for (let _0x177aa8 = 0; _0x177aa8 < _0x52661e.length; _0x177aa8++) {
-            const _0x407421 = _0x19edc6.constructor.prototype.bind(_0x19edc6);
-            const _0x11b9b6 = _0x52661e[_0x177aa8];
-            const _0x132571 = _0x48f774[_0x11b9b6] || _0x407421;
-            _0x407421.__proto__ = _0x19edc6.bind(_0x19edc6);
-            _0x407421.toString = _0x132571.toString.bind(_0x132571);
-            _0x48f774[_0x11b9b6] = _0x407421;
-        }
-    });
-    _0x1e2750();
-    const _0x51af79 = crypto.getRandomValues(new Uint8Array(12));
+    const _0x51af79 = crypto.getRandomValues(new Uint8Array(12))
     const _0x43ecbe = {
-        name: "AES-GCM",
-        length: 0x100
-    };
-    const _0x248f16 = await crypto.subtle.generateKey(_0x43ecbe, true, ["encrypt", "decrypt"]);
-    const _0x28a5d8 = await crypto["subtl" + "e"].exportKey("raw", _0x248f16);
-    const _0x2ac056 = await _0x3a0d9f(_0x28a5d8, _0x36daa1);
-    const _0xb15ea2 = JSON.stringify(_0x320e1f);
-    const _0x3157cb = new TextEncoder().encode(_0xb15ea2);
+        name: 'AES-GCM',
+        length: 0x100,
+    }
+    const _0x248f16 = await crypto.subtle.generateKey(_0x43ecbe, true, ['encrypt', 'decrypt'])
+    const _0x28a5d8 = await crypto.subtle.exportKey('raw', _0x248f16)
+    const _0x2ac056 = await _0x3a0d9f(_0x28a5d8, _0x36daa1)
+    const _0xb15ea2 = JSON.stringify(_0x320e1f)
+    const _0x3157cb = new TextEncoder().encode(_0xb15ea2)
     const _0x2201b2 = {
-        name: "AES-GCM",
-        iv: _0x51af79
-    };
-    const _0x530cff = await crypto["subtl" + "e"].encrypt(_0x2201b2, _0x248f16, _0x3157cb);
-    const _0x49cdde = btoa(String.fromCharCode(...new Uint8Array(_0x530cff)));
-    const _0x18f45b = btoa(String.fromCharCode(...new Uint8Array(_0x51af79)));
-    const _0x4db5ae = btoa(String.fromCharCode(...new Uint8Array(_0x2ac056)));
-    return [_0x49cdde, _0x18f45b, _0x4db5ae];
+        name: 'AES-GCM',
+        iv: _0x51af79,
+    }
+    const _0x530cff = await crypto.subtle.encrypt(_0x2201b2, _0x248f16, _0x3157cb)
+    const _0x49cdde = btoa(String.fromCharCode(...new Uint8Array(_0x530cff)))
+    const _0x18f45b = btoa(String.fromCharCode(...new Uint8Array(_0x51af79)))
+    const _0x4db5ae = btoa(String.fromCharCode(...new Uint8Array(_0x2ac056)))
+    return [_0x49cdde, _0x18f45b, _0x4db5ae]
 }
 async function _0x3a0d9f(_0x4cd735, _0x2a3f96) {
     const _0x28fa55 = {
-        "name": "SHA-256"
-    };
+        name: 'SHA-256',
+    }
     const _0x15ec17 = {
-        "name": "RSA-OAEP"
-    };
-    _0x15ec17.hash = _0x28fa55;
-    const _0x2358ee = await crypto.subtle.importKey("jwk", _0x2a3f96, _0x15ec17, true, ["encrypt"]);
+        name: 'RSA-OAEP',
+    }
+    _0x15ec17.hash = _0x28fa55
+    const _0x2358ee = await crypto.subtle.importKey('jwk', _0x2a3f96, _0x15ec17, true, ['encrypt'])
     const _0x7b1865 = {
-        name: "RSA-OAEP"
-    };
-    return await crypto["subtl" + "e"].encrypt(_0x7b1865, _0x2358ee, _0x4cd735);
+        name: 'RSA-OAEP',
+    }
+    return await crypto.subtle.encrypt(_0x7b1865, _0x2358ee, _0x4cd735)
 }
-const _0x41f1df = performance.now();
-const _0x51dd0d = document.createElement("canvas");
-if (typeof _0x51dd0d.getContext !== "function") { }
-const _0x20122e = _0x51dd0d.getContext("webgl");
-if (_0x20122e === null) { }
-if (typeof _0x20122e.getParameter !== "function") { }
-const _0x267ecf = _0x20122e.getParameter(_0x20122e.VENDOR);
-const _0x5af2cc = _0x20122e.getParameter(_0x20122e.RENDERER);
-const _0x5bdda3 = _0x20122e ? _0x20122e["getEx" + "tensi" + "on"]("WEBGL_debug_renderer_info") : null;
-let _0x27d783 = null;
-if (_0x5bdda3) {
-    _0x27d783 = _0x20122e.getParameter(_0x5bdda3.UNMASKED_RENDERER_WEBGL);
+const __canvas = document.createElement('canvas')
+if (typeof __canvas.getContext !== 'function') {
 }
-const _0x57f51a = {
-    vendor: _0x267ecf,
-    renderer: _0x5af2cc,
-    unmaskedRenderer: _0x27d783
-};
-_0x4dc979(_0x57f51a).then(_0x4d3de6 => {
-    localStorage.setItem("t10", JSON.stringify(_0x4d3de6));
-    const _0x5f32d8 = new Event("dataChanged");
-    window["dispatchEv" + "ent"](_0x5f32d8);
-})["catch"](() => {
-});
+const __webglContext = __canvas.getContext('webgl')
+if (__webglContext === null) {
+}
+if (typeof __webglContext.getParameter !== 'function') {
+}
+const __gpuVendor = __webglContext.getParameter(__webglContext.VENDOR)
+const __gpuRenderer = __webglContext.getParameter(__webglContext.RENDERER)
+const __WEBGL_debug_renderer_info = __webglContext ? __webglContext.getExtension('WEBGL_debug_renderer_info') : null
+let __unmaskedRenderer = null
+if (__WEBGL_debug_renderer_info) {
+    __unmaskedRenderer = __webglContext.getParameter(__WEBGL_debug_renderer_info.UNMASKED_RENDERER_WEBGL)
+}
+const result = {
+    vendor: __gpuVendor,
+    renderer: __gpuRenderer,
+    unmaskedRenderer: __unmaskedRenderer,
+}
+_0x4dc979(result)
+    .then((_0x4d3de6) => {
+        localStorage.setItem('t10', JSON.stringify(_0x4d3de6))
+        const _0x5f32d8 = new Event('dataChanged')
+        window.dispatchEvent(_0x5f32d8)
+    })
+['catch'](() => { })
--- 1.8.8/detections/VMs/t8.js
+++ 1.8.8/detections/VMs/t8.js
@@ -6,82 +6,94 @@ const _0x2e3322 = {
     ext: true,
     key_ops: ['encrypt'],
     kty: 'RSA',
-    n: "8DJNossKBi-kNHAmIckUEs2ceEm32xlIwjqEtJ4gtxsLHdSa8HLs2SM6tFvxLQCkq9B_dWkZ1U1B0iH5oy4EaAOqC-NhZDh_UWTLRvoj9bHFFGFhRTOG80ztY-aeGuSeE5q3k86t9dgBA3v-BBW9u_ISuOd_hJvb6Z8kZmheak74uXRFTKkjO9SawZa-9cKC82lucYQeHPBWiqg7F8o15TSKtBqa8KD5_RYel3BZpUaN1mdQIO0HHiFbxeb2lLXcPlXL1cSGD1bOeQJ75i_0Sl50DHI6vM7SZpxRYc6dVO4trG05bGfNoQtmbww_UXzcOEi4LZI-4Ed42oTQxP3rTw"
-};
+    n: '8DJNossKBi-kNHAmIckUEs2ceEm32xlIwjqEtJ4gtxsLHdSa8HLs2SM6tFvxLQCkq9B_dWkZ1U1B0iH5oy4EaAOqC-NhZDh_UWTLRvoj9bHFFGFhRTOG80ztY-aeGuSeE5q3k86t9dgBA3v-BBW9u_ISuOd_hJvb6Z8kZmheak74uXRFTKkjO9SawZa-9cKC82lucYQeHPBWiqg7F8o15TSKtBqa8KD5_RYel3BZpUaN1mdQIO0HHiFbxeb2lLXcPlXL1cSGD1bOeQJ75i_0Sl50DHI6vM7SZpxRYc6dVO4trG05bGfNoQtmbww_UXzcOEi4LZI-4Ed42oTQxP3rTw',
+}
 async function _0x36fa49(_0x98d040) {
-    const _0x447d35 = crypto.getRandomValues(new Uint8Array(12));
+    const _0x447d35 = crypto.getRandomValues(new Uint8Array(12))
     const _0x4f819d = {
         name: 'AES-GCM',
-        length: 0x100
-    };
-    const _0x2c4203 = await crypto.subtle.generateKey(_0x4f819d, true, ['encrypt', 'decrypt']);
-    const _0x42083c = await crypto.subtle.exportKey('raw', _0x2c4203);
-    const _0xebd815 = await _0x34f1c8(_0x42083c, _0x2e3322);
-    const _0x222c54 = JSON.stringify(_0x98d040);
-    const _0x162a94 = new TextEncoder().encode(_0x222c54);
+        length: 0x100,
+    }
+    const _0x2c4203 = await crypto.subtle.generateKey(_0x4f819d, true, ['encrypt', 'decrypt'])
+    const _0x42083c = await crypto.subtle.exportKey('raw', _0x2c4203)
+    const _0xebd815 = await _0x34f1c8(_0x42083c, _0x2e3322)
+    const _0x222c54 = JSON.stringify(_0x98d040)
+    const _0x162a94 = new TextEncoder().encode(_0x222c54)
     const _0x53d363 = {
         name: 'AES-GCM',
-        iv: _0x447d35
-    };
-    const _0x53ae1c = await crypto.subtle.encrypt(_0x53d363, _0x2c4203, _0x162a94);
-    const _0x4efb0c = btoa(String.fromCharCode(...new Uint8Array(_0x53ae1c)));
-    const _0x599aab = btoa(String.fromCharCode(...new Uint8Array(_0x447d35)));
-    const _0x1af336 = btoa(String.fromCharCode(...new Uint8Array(_0xebd815)));
-    return [_0x4efb0c, _0x599aab, _0x1af336];
+        iv: _0x447d35,
+    }
+    const _0x53ae1c = await crypto.subtle.encrypt(_0x53d363, _0x2c4203, _0x162a94)
+    const _0x4efb0c = btoa(String.fromCharCode(...new Uint8Array(_0x53ae1c)))
+    const _0x599aab = btoa(String.fromCharCode(...new Uint8Array(_0x447d35)))
+    const _0x1af336 = btoa(String.fromCharCode(...new Uint8Array(_0xebd815)))
+    return [_0x4efb0c, _0x599aab, _0x1af336]
 }
 async function _0x34f1c8(_0x43ca6f, _0x261bad) {
     const _0x5be6e8 = {
-        name: 'SHA-256'
-    };
+        name: 'SHA-256',
+    }
     const _0x184595 = {
         name: 'RSA-OAEP',
-        hash: _0x5be6e8
-    };
-    const _0xc84dd9 = await crypto.subtle.importKey('jwk', _0x261bad, _0x184595, true, ['encrypt']);
+        hash: _0x5be6e8,
+    }
+    const _0xc84dd9 = await crypto.subtle.importKey('jwk', _0x261bad, _0x184595, true, ['encrypt'])
     const _0x338572 = {
-        name: "RSA-OAEP"
-    };
-    return await crypto.subtle.encrypt(_0x338572, _0xc84dd9, _0x43ca6f);
+        name: 'RSA-OAEP',
+    }
+    return await crypto.subtle.encrypt(_0x338572, _0xc84dd9, _0x43ca6f)
 }
 const _0x52b70b = async () => {
-    const _0xbb1300 = document.createElement('input');
-    _0xbb1300.type = 'text';
-    _0xbb1300.id = 'sd1';
-    _0xbb1300.value = 'bla';
-    _0xbb1300.style.visibility = 'hidden';
-    document.body.appendChild(_0xbb1300);
-    const _0x3c268b = document.querySelector('#sd1');
-    const _0x45c175 = _0x3c268b.selectionDirection;
-    const _0x3331e4 = _0x45c175 === 'none' ? 0 : 1;
-    let _0x30baf7;
+    const __inputElem = document.createElement('input')
+    __inputElem.type = 'text'
+    __inputElem.id = 'sd1'
+    __inputElem.value = 'bla'
+    __inputElem.style.visibility = 'hidden'
+    document.body.appendChild(__inputElem)
+    const __inputElemRef = document.querySelector('#sd1')
+    const __selectionDirection = __inputElemRef.selectionDirection
+    const __platform = __selectionDirection === 'none' ? 0 : 1
+    let __clientHints
     if (!navigator.userAgentData || !navigator.userAgentData.getHighEntropyValues) {
-        _0x30baf7 = {};
+        __clientHints = {}
     } else {
-        const _0x4613dd = await navigator.userAgentData.getHighEntropyValues(['platform', 'platformVersion', 'architecture', 'bitness', 'model', "uaFullVersion", 'fullVersionList', "formFactor", 'wow64']);
-        const {
-            mobile: _0x4165ae
-        } = navigator.userAgentData || {};
+        const _0x4613dd = await navigator.userAgentData.getHighEntropyValues([
+            'platform',
+            'platformVersion',
+            'architecture',
+            'bitness',
+            'model',
+            'uaFullVersion',
+            'fullVersionList',
+            'formFactor',
+            'wow64',
+        ])
+        const { mobile: _0x4165ae } = navigator.userAgentData || {}
         if (!_0x4613dd.mobile) {
-            _0x4613dd.mobile = _0x4165ae;
+            _0x4613dd.mobile = _0x4165ae
         }
-        _0x30baf7 = Object.keys(_0x4613dd).sort().reduce((_0x40effe, _0xe4f32) => {
-            _0x40effe[_0xe4f32] = _0x4613dd[_0xe4f32];
-            return _0x40effe;
-        }, {});
+        __clientHints = Object.keys(_0x4613dd)
+            .sort()
+            .reduce((_0x40effe, _0xe4f32) => {
+                _0x40effe[_0xe4f32] = _0x4613dd[_0xe4f32]
+                return _0x40effe
+            }, {})
     }
-    const _0x2ad94f = {
-        platform: _0x3331e4
-    };
-    _0x2ad94f.clientHints = _0x30baf7;
-    console.log('t8', _0x2ad94f);
+    const result = {
+        platform: __platform,
+    }
+    result.clientHints = __clientHints
+    console.log('t8', result)
     const _0x3750d7 = {
-        platform: _0x3331e4,
-        clientHints: _0x30baf7
-    };
-    _0x36fa49(_0x3750d7).then(_0xab8fd6 => {
-        localStorage.setItem('t8', JSON.stringify(_0xab8fd6));
-        const _0x32b6e8 = new Event("dataChanged");
-        window.dispatchEvent(_0x32b6e8);
-    })['catch'](() => { });
-};
-_0x52b70b();
+        platform: __platform,
+        clientHints: __clientHints,
+    }
+    _0x36fa49(_0x3750d7)
+        .then((_0xab8fd6) => {
+            localStorage.setItem('t8', JSON.stringify(_0xab8fd6))
+            const _0x32b6e8 = new Event('dataChanged')
+            window.dispatchEvent(_0x32b6e8)
+        })
+    ['catch'](() => { })
+}
+_0x52b70b()
